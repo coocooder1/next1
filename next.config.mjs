e51558b349904f8d05c03784097d4c4b91626f3f
coocooder1 <elvs97121041@gmail.com>
2024-09-07 16:08:45 +0800

    add image3

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
+    output: "export",
     reactStrictMode: true,
     images: {
-        unoptimized: true, // 禁用图片优化
-      },
-
-};
-
+      unoptimized: true, // 禁用图片优化
+    },
+    // 这里替换为你的仓库名，例如 'my-repo'
+    basePath: '/next1', 
+    assetPrefix: '/next1/', // 静态资源路径前缀
+  };
 
 export default nextConfig;
