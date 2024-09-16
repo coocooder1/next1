2b597cd7b9af173278a645dfc7cf915ab672f70a
coocooder1 <elvs97121041@gmail.com>
2024-09-16 12:17:49 +0800

    add image25

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -2,65 +2,30 @@
 import React from 'react'
 import Image from 'next/image'
 import { Fade } from 'react-awesome-reveal'
-
+import Link from 'next/link';
 export default function Sign() {
     return (
       <main>
-        <div id="home-section" className='bg-lightpink'>
-      <div className='mx-auto max-w-7xl pt-20 sm:pb-24 px-6'>
-        <div className=' grid grid-cols-1 lg:grid-cols-12  space-x-1'>
-          {/* {Lefr Section} */}
-          <div className='col-span-6 flex flex-col justify-center'>
-            <Fade direction='up' delay={400} cascade damping={1e-1} triggerOnce={true} >
-              <h1 className='text-4xl lg:text-7xl font-semibold mb-5 text-lightgrey md:4px lg:text-start text-center'>
-                Toyota 86 <br/>車聚
-              </h1>
-
-            </Fade>
-            <Fade direction='up' delay={800} cascade damping={1e-1} triggerOnce={true}>
-            <p className='text-grey lg:text-lg font-normal  mb-10 lg:text-start text-center'>
-              拍攝日期在2024年9月 <br/> 
-            </p>
-              
-            </Fade>
-            <Fade direction='up' delay={1000} cascade damping={1e-1} triggerOnce={true}>
-            <div className='md:flex align-middle justify-center lg:justify-start'>
-              <button className='text-xl w-full bg-pink  text-white md:w-auto rounded-full px-4 py-3 lg:px-14 mr-6'> GO</button>
-              {/* <button className=' flex border w-full md:w-auto mt-5 md:mt-0 items-center py-5 px-10 
-              rounded-full border-pink justify-center text-pink hover:text-white hover:bg-pink'>Expolore now</button> */}
-            </div>
-
-            </Fade>
-            
-            
-            
-          </div>
-          {/* {Right Section} */}
-          <div className='col-span-6 flex flex-col justify-center sm:mt-5'>
-            <div className='flex bg-white p-2 gap-5 items-center bottom-2 left-50 rounded-xl absolute '>
-                  {/* <Image 
-                  src="/car.svg"
-                  alt="car"
-                  width={68}
-                  height={68}/> */}
-                </div>
-                <Fade direction='right' delay={1000} cascade damping={1e-1} triggerOnce={true}>
-                  <div >
-                    <Image 
-                    src={`${process.env.NEXT_PUBLIC_BASE_PATH}/86.jpg`}
-                    alt="car"
-                    width={1000}
-                    height={805}/>
-                  </div>
-
-                </Fade>
-                
-
-          </div>
-              
-              
-        </div>
+        
+        <div className='p-3 max-w-lg mx-auto' >
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign in</h1>
+      <form  className='flex flex-col gap-4'>
+       
+        <input type='email' placeholder='email' id='email' 
+        className='bg-slate-100 rounded-lg p-3'/>
+        <input type='password' placeholder='password' id='password' 
+        className='bg-slate-100 rounded-lg p-3'/>
+        <button className='bg-slate-500 rounded-lg text-white p-3 uppercase 
+        hover:opacity-75'></button>
+   
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Don't Have an account?</p>
+        <Link href='/'>
+        <span className='text-blue-500'>Sign up</span>
+        </Link>
       </div>
+   
     </div>
       </main>
     )
