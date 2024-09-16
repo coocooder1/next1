d2bd69b2f6d3b081c21fb6137ae16f5acb6ba6dd
coocooder1 <elvs97121041@gmail.com>
2024-09-16 12:21:20 +0800

    add image25

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -8,23 +8,16 @@ export default function Sign() {
       <main>
         
         <div className='p-3 max-w-lg mx-auto' >
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign in</h1>
-      <form  className='flex flex-col gap-4'>
-       
-        <input type='email' placeholder='email' id='email' 
-        className='bg-slate-100 rounded-lg p-3'/>
-        <input type='password' placeholder='password' id='password' 
-        className='bg-slate-100 rounded-lg p-3'/>
-        <button className='bg-slate-500 rounded-lg text-white p-3 uppercase 
-        hover:opacity-75'></button>
-   
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Don't Have an account?</p>
-        <Link href='/'>
-        <span className='text-blue-500'>Sign up</span>
-        </Link>
-      </div>
+       <h1 className='text-3xl text-center font-semibold my-7'>Sign in</h1>
+       <form  className='flex flex-col gap-4'>
+
+         <input type='email' placeholder='email' id='email' 
+         className='bg-slate-100 rounded-lg p-3'/>
+         <input type='password' placeholder='password' id='password' 
+         className='bg-slate-100 rounded-lg p-3'/>
+         <button className='bg-slate-500 rounded-lg text-white p-3 uppercase 
+         hover:opacity-75'></button>
+          </form>
    
     </div>
       </main>
