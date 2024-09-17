5988c9fad01e34274adde507b1e610a8aa229c91
coocooder1 <elvs97121041@gmail.com>
2024-09-17 11:28:33 +0800

    add image26

--- app/camera/page.tsx
+++ app/camera/page.tsx
@@ -5,8 +5,8 @@ const camera = () => {
     <div className='' id='gallery-section'>
       <div className='ma-auto max-w-2xl lg:max-w-7xl mt-36'>
         <div className='text-center'>
-          <h2 className='text-pink text-lg font-normal mb-3'>作品</h2>
-          <h3 className='text-3xl lg:text-5xl font-semibold text-black'> 車子作品</h3>
+          <h2 className='text-pink text-lg font-normal mb-3'>111 Track Day Gmotor </h2>
+          <h3 className='text-3xl lg:text-5xl font-semibold text-black'> </h3>
         </div>
         {/* {Gallery Photos} */}
         <div className='grid grid-cols-1  md:grid-cols-12 my-16 sm:space-x-6 sapce-y-6 md:space-y-0 px-6 '>
@@ -15,7 +15,7 @@ const camera = () => {
               {/* {Top photo} */}
               <div className='row-span-1   mb-5 mt-5' >
                 <Image 
-                src={`${process.env.NEXT_PUBLIC_BASE_PATH}/867.jpg`}
+                src={`${process.env.NEXT_PUBLIC_BASE_PATH}/carr1.jpg`}
                 alt="86"
                 width={960}
                 height={405}
@@ -27,7 +27,7 @@ const camera = () => {
               <div className='grid grid-cols-2 gap-2 '>
               <div >
                   <Image 
-                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/864.jpg`}
+                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/carr4.jpg`}
                   alt="86"
                   width={450}
                   height={405}
@@ -37,7 +37,7 @@ const camera = () => {
                 </div>
                 <div >
                   <Image 
-                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/865.jpg`}
+                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/carr3.jpg`}
                   alt="86"
                   width={450}
                   height={300}
@@ -47,7 +47,7 @@ const camera = () => {
                 </div>
                 <div >
                   <Image 
-                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/863.jpg`}
+                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/carr2.jpg`}
                   alt="86"
                   width={450}
                   height={300}
@@ -57,7 +57,7 @@ const camera = () => {
                 </div>
                 <div >
                   <Image 
-                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/8610.jpg`}
+                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/carr5.jpg`}
                   alt="86"
                   width={450}
                   height={300}
